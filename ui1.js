8f6c950a783a78143b012420cc7cd5fc5794adb4
vivek7005 <89511196+vivek7005@users.noreply.github.com>
2021-08-27 18:35:23 +0530

    Update ui1.js

--- ui1.js
+++ ui1.js
@@ -1,1 +1,49 @@
-
+class UI {
+    constructor() {
+      this.uiContainer = document.getElementById("content");
+      this.city;
+      this.defaultCity = "London";
+    }
+  
+    populateUI(data) {
+      //de-structure vars
+  
+      //add them to inner HTML
+  
+      this.uiContainer.innerHTML = `
+          
+          <div class="card mx-auto mt-5 bg-secondary text-white text-center" style="width: 18rem;">
+              <div class="card-body justify-content-center fas fa-cloud fa-2x">
+                  <h5 class="card-title">${data.name}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted ">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min}</h6>
+                  <p class="card-text ">Weather condition: ${data.weather[0].description}</p>
+                  
+              </div>
+          </div>
+          
+          
+          `;
+    }
+  
+    clearUI() {
+      uiContainer.innerHTML = "";
+    }
+  
+    saveToLS(data) {
+      localStorage.setItem("city", JSON.stringify(data));
+    }
+  
+    getFromLS() {
+      if (localStorage.getItem("city" == null)) {
+        return this.defaultCity;
+      } else {
+        this.city = JSON.parse(localStorage.getItem("city"));
+      }
+  
+      return this.city;
+    }
+  
+    clearLS() {
+      localStorage.clear();
+    }
+  }
